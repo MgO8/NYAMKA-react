b1365469dbdd04b1c542e4f7c0b7bbf0ff4784da
Margo <margo@meowmeow>
2021-12-13 17:09:34 +0700

    nyamka to react wip

--- src/components/List.js
+++ src/components/List.js
@@ -1,50 +1,71 @@
 import { Link } from "react-router-dom";
+import Axios from 'axios';
+
+import { useState, useEffect } from 'react';
 
 export default function List() {
-  return (
-    <div className="List">
-        <div class="container">
-        <div class="container-header">
-            <h1>Daily Food List</h1>
-            <Link to="/new" className="new-button">
-            +Add item
-            </Link>
-        </div>
-        <div class="index-table-wrapper">
-            <div class="table-head">
-                <span class="id-column">ID</span>
-                <span class="name-column">Item</span>
-                <span class="price-column">Price</span>
-                <span class="date-column">Date</span>
-            </div>
-            <ul class="table-body">
-                {/* <% items.forEach((item)=> { %> */}
-                    <li>
-                        <div class="item-data">
-                            <span class="id-column">
-                                {/* <%= item.id %> */}
-                            </span>
-                            <span class="name-column">
-                                {/* <%= item.name %> */}
-                            </span>
-                            <span class="price-column">
-                                {/* <%= item.price %> */}
-                            </span>
-                            <span class="date-column">
-                                {/* <%= item.date %> */}
-                            </span>
-                        </div>
-                        <div class="item-menu">
-                            {/* <form action="/delete/<%= item.id %>" method="post">
+    const [foodList, setFoodList] = useState([]);
+
+    const getFoodList = () => {
+        Axios.get("http://localhost:3001/list").then((response) => {
+            console.log(response.data.items);
+            setFoodList(response.data.items)
+        })
+    };
+
+    useEffect(() => {
+        getFoodList(foodList);
+      }, []);
+
+    const itemList = foodList.map(foodItem => {
+        return (<li><span>foodItem.id</span></li>)
+    })
+
+    return (
+        <div className="List">
+            <div class="container">
+                <div class="container-header">
+                    <h1>Daily Food List</h1>
+                    <Link to="/new" className="new-button">
+                        +Add item
+                    </Link>
+                </div>
+                <div class="index-table-wrapper">
+                    <div class="table-head">
+                        <span class="id-column">ID</span>
+                        <span class="name-column">Item</span>
+                        <span class="price-column">Price</span>
+                        <span class="date-column">Date</span>
+                    </div>
+                    <ul class="table-body">
+                        <itemList></itemList>
+                        {/* <% items.forEach((item)=> { %> */}
+                        <li>
+                            <div class="item-data">
+                                <span class="id-column">
+                                    {/* <%= item.id %> */}
+                                </span>
+                                <span class="name-column">
+                                    {/* <%= item.name %> */}
+                                </span>
+                                <span class="price-column">
+                                    {/* <%= item.price %> */}
+                                </span>
+                                <span class="date-column">
+                                    {/* <%= item.date %> */}
+                                </span>
+                            </div>
+                            <div class="item-menu">
+                        {/* <form action="/delete/<%= item.id %>" method="post">
                                 <input type="submit" value="Delete">
                             </form>
                             <a href="/edit/<%= item.id %>">Edit</a> */}
-                        </div>
-                    </li>
-                    {/* <% }) %> */}
-            </ul>
+                            </div>
+                        </li>
+                        {/* <% }) %> */}
+                    </ul>
+                </div>
+            </div>
         </div>
-    </div>
-    </div>
-  );
+    );
 }
--- src/components/New.js
+++ src/components/New.js
@@ -1,26 +1,57 @@
 import { Link } from "react-router-dom";
+import { useState } from 'react';
+import Axios from 'axios';
 
 export default function New() {
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState('');
+  const [date, setDate] = useState('');
+  // const [items, setItems] = useState([]);
+
+  const addItem = () => {
+    Axios.post("http://localhost:3001/create", {
+      name: name, 
+      price: price, 
+      date: date
+    }).then(() => {
+      console.log('success');
+    }).catch((e) => {
+      console.log(e)
+    })
+  };
+
+  // const addItem = () => {
+  //   const newItem = { name, price, date };
+  //   setItems([newItem, ...items]);
+  // };
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    addItem();
+  }
+
   return (
     <div className="new">
-      <div class="container">
-        <div class="container-header">
+      <div className="container">
+        <div className="container-header">
           <h1>Add Item</h1>
         </div>
-        <div class="item-form-wrapper">
-          {/*             
-            <form action="/create" method="post">
-                <p class="form-label">Food name</p>
-                <input name="itemName" type="text" required>
-                <p class="form-label">Price</p>
-                <input name="itemPrice" type="number" max="1000000" required>
-                <p class="form-label" type="text">Date</p>
-                <input name="date" type="date" required>
-                <input type="submit" value="Add">
-            </form> */}
+        <div className="item-form-wrapper">
+          {/* <form action="/create" method="post">    */}
+          <form onSubmit={handleSubmit}>
+            <p className="form-label">Food name</p>
+            <input value={name} onChange={(e) => setName(e.currentTarget.value)} type="text" required />
+            <p className="form-label">Price</p>
+            <input value={price} onChange={(e) => setPrice(e.currentTarget.value)} type="number"
+              max="1000000" required />
+            <p className="form-label" type="text">Date</p>
+            <input value={date} onChange={(e) => setDate(e.currentTarget.value)} type="date" required />
+            Date
+            <button type="submit">Add</button>
+          </form>
         </div>
         <Link to="/list" className="cancel-button">
-          Go back
+          Go back 
         </Link>
       </div>
     </div>
